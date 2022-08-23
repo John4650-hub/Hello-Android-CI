d0a94d726bd00632663f7a5bbff206d0a1c2c7b7
johndelvin51@gmail.com <johndelvin51@gmail.com>
2022-08-24 02:30:00 +0300

    done

--- mygit.sh
+++ mygit.sh
@@ -1,4 +1,3 @@
-sudo apt install git
 sudo mkdir /myArtifacts
 cd myArtifacts
-sudo git clone https://github.com/codepath/android_guides.wiki.git
+git clone https://github.com/codepath/android_guides.wiki.git
